37c7d4d6d920a357804d6371739fac4ddae0caa7
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2025-07-14 16:35:01 -0700

    reimplement `keep_alive` using an MPSC

--- src/utils/keep_alive.rs
+++ src/utils/keep_alive.rs
@@ -1,9 +1,10 @@
 use std::{
     fmt,
-    ptr::{self, NonNull},
+    ptr::NonNull,
     sync::{
         Arc,
-        atomic::{AtomicPtr, AtomicUsize, Ordering::*},
+        atomic::{AtomicUsize, Ordering::*},
+        mpsc,
     },
 };
 
@@ -27,14 +28,14 @@ struct KeepAliveListInner<T: Copy> {
     /// [`KeepAliveList`] while it's being borrowed mutably.
     bump: Bump,
 
-    /// The first [`KeepAliveSlot`] in the list of potentially destroyed slots. These slots are only
-    /// actually considered destroyed if the reference count at the time of iteration is actually
-    /// zero. This is because users can resurrect dead slots using the [`KeepAliveList::upgrade`]
-    /// method.
+    /// A sender for potentially condemned slots.
+    sender: mpsc::Sender<KeepAlivePtr<T>>,
+
+    /// A receiver for potentially condemned slots.
     ///
-    /// This pointee is either [`KeepAliveList::SLOT_TERMINAL`] or a valid pointee guaranteed to be
-    /// live for the duration of this [`KeepAliveListInner`] instance.
-    head: AtomicPtr<KeepAliveSlot<T>>,
+    /// To avoid misuse of the `!Sync` nature of `mpsc::Receiver`, this value must only be accessed
+    /// by [`KeepAliveList`] while it's being borrowed mutably.
+    receiver: mpsc::Receiver<KeepAlivePtr<T>>,
 }
 
 struct KeepAliveSlot<T: Copy> {
@@ -47,17 +48,6 @@ struct KeepAliveSlot<T: Copy> {
 
     /// The number of [`KeepAliveStrong`]s pointing to this slot.
     refs: AtomicUsize,
-
-    /// The next potentially destroyed slot in the linked list. See [`KeepAliveListInner::head`] for
-    /// details on how to interpret this list.
-    ///
-    /// This pointer is [`KeepAliveList::SLOT_NOT_IN_LIST`] if the slot is not in the linked list,
-    /// [`KeepAliveList::SLOT_TERMINAL`] if it's the last slot in the list, and a pointer to a
-    /// [`KeepAliveSlot`] if it's in the list but not terminal.
-    ///
-    /// This pointer is only ever mutated by [`KeepAliveStrong::drop`] and
-    /// [`KeepAliveList::take_condemned`].
-    next: AtomicPtr<KeepAliveSlot<T>>,
 
     /// User-defined information about the slot.
     userdata: T,
@@ -75,26 +65,25 @@ impl<T: Copy> fmt::Debug for KeepAliveList<T> {
 
 impl<T: Copy> Default for KeepAliveList<T> {
     fn default() -> Self {
+        let (sender, receiver) = mpsc::channel();
+
         Self {
             inner: Arc::new(KeepAliveListInner {
                 bump: Bump::new(),
-                head: AtomicPtr::new(Self::SLOT_TERMINAL as *mut _),
+                sender,
+                receiver,
             }),
         }
     }
 }
 
 impl<T: Copy> KeepAliveList<T> {
-    const SLOT_NOT_IN_LIST: *mut KeepAliveSlot<T> = ptr::null_mut();
-    const SLOT_TERMINAL: *mut KeepAliveSlot<T> = 0x1 as *mut _;
-
     /// Create a new slot with the supplied `userdata` and return a unique [`KeepAliveStrong`]
     /// reference to it.
     pub fn spawn(&mut self, userdata: T) -> KeepAliveStrong<T> {
         let cell = self.inner.bump.alloc(KeepAliveSlot {
             owner: Arc::into_raw(self.inner.clone()),
             refs: AtomicUsize::new(1),
-            next: AtomicPtr::new(Self::SLOT_NOT_IN_LIST),
             userdata,
         });
 
@@ -126,39 +115,15 @@ impl<T: Copy> KeepAliveList<T> {
     /// observed.
     pub fn take_condemned(&mut self) -> Option<(KeepAlivePtr<T>, T)> {
         loop {
-            // Advance the head to the next element.
-            let res = self.inner.head.fetch_update(Release, Acquire, |head| {
-                debug_assert_ne!(head, Self::SLOT_NOT_IN_LIST);
+            let next = self.inner.receiver.recv().ok()?;
 
-                if head == Self::SLOT_TERMINAL {
-                    // This is the terminator of the list.
-                    return None;
-                }
-
-                // Safety: `head` is valid for the duration of its owner, which is `self`.
-                let slot = unsafe { &*head };
-
-                Some(slot.next.load(Relaxed))
-            });
-
-            match res {
-                Ok(prev_head) => {
-                    let slot_ptr = KeepAlivePtr(unsafe { NonNull::new_unchecked(prev_head) });
-                    let slot = unsafe { slot_ptr.0.as_ref() };
-
-                    slot.next.store(Self::SLOT_NOT_IN_LIST, Relaxed);
-
-                    if slot.refs.load(Relaxed) > 0 {
-                        // This slot was a false positive.
-                        continue;
-                    }
-
-                    break Some((slot_ptr, slot.userdata));
-                }
-                Err(_) => {
-                    break None;
-                }
+            if unsafe { next.0.as_ref() }.refs.load(Relaxed) > 0 {
+                // This cannot miss events because we always send a new event to the MPSC when the
+                // reference count becomes zero.
+                continue;
             }
+
+            return Some((next, unsafe { next.userdata() }));
         }
     }
 }
@@ -225,7 +190,6 @@ impl<T: Copy> Clone for KeepAliveStrong<T> {
 
 impl<T: Copy> Drop for KeepAliveStrong<T> {
     fn drop(&mut self) {
-        let slot_ptr = self.ptr.0.as_ptr();
         let slot = self.slot();
 
         // Decrement reference count.
@@ -234,22 +198,8 @@ impl<T: Copy> Drop for KeepAliveStrong<T> {
             return;
         }
 
-        // Push the slot to the linked list of potentially destroyed slots.
-        {
-            let head = unsafe { &(*slot.owner).head };
-
-            _ = head.fetch_update(Release, Acquire, |head| {
-                if slot.next.load(Relaxed) != KeepAliveList::<T>::SLOT_NOT_IN_LIST {
-                    // (already in list)
-                    return None;
-                }
-
-                // Update the head.
-                slot.next.store(head, Relaxed);
-
-                Some(slot_ptr)
-            });
-        }
+        // Tell the MPSC about our death.
+        unsafe { &*slot.owner }.sender.send(self.ptr).unwrap();
 
         // Decrement the reference count for the owner structure.
         drop(unsafe { Arc::from_raw(slot.owner) });
